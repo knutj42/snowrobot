ead4a5594f48af7a0f97a78bb6d3d5c4000bb82f
Knut Johannessen <knut.johannessen@bouvet.no>
2018-04-16 19:33:30 +0200

    moved the webapp folder

--- remotecontrol/server/build_and_push_docker.sh
+++ remotecontrol/server/build_and_push_docker.sh
@@ -1,1 +1,11 @@
-docker build -t knutj42/robots-controlcenter . && docker push knutj42/robots-controlcenter
+#!/usr/bin/env bash
+set -e
+pushd webapp
+#npm run build
+rm -rf ../controlcenter/controlcenter/static/*
+cp -a build/static/*  ../controlcenter/controlcenter/static/
+cp build/index.html  ../controlcenter/controlcenter/templates/index.html
+popd
+
+docker build -t knutj42/robots-controlcenter .
+docker push knutj42/robots-controlcenter

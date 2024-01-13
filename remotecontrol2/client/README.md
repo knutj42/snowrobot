7893670aa270eb2ba699a4a4477ed2273b2088be
Knut Johannessen <knutj42@gmail.com>
2024-01-13 21:10:02 +0100

    test client that plays back a file from a webpage (but that doesn't shut down when the video ends)

--- remotecontrol2/client/README.md
+++ remotecontrol2/client/README.md
@@ -4,6 +4,6 @@ This folder contains the remote control application that run on a windows laptop
 Howto build the client on Windows:
 * Install MSYS2 (https://www.msys2.org/).
 * Run the following command in the "MSYS2 UCRT64" terminal to install the required packages:
-  * pacman -S mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja mingw-w64-ucrt-x86_64-gtk4-media-gstreamer glib2-devel
+  * pacman -S mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja mingw-w64-ucrt-x86_64-gtk4-media-gstreamer glib2-devel mingw-w64-ucrt-x86_64-gst-plugins-good mingw-w64-ucrt-x86_64-gst-plugins-bad mingw-w64-ucrt-x86_64-gst-plugins-ugly
 
 In the 
--- remotecontrol2/client/client.cpp
+++ remotecontrol2/client/client.cpp
@@ -1,26 +1,51 @@
-#include <stdio.h>
+#include <iostream>
 #include <gst/gst.h>
 
 int
-main (int   argc,
-      char *argv[])
+main (int argc, char *argv[])
 {
-  const gchar *nano_str;
-  guint major, minor, micro, nano;
+   g_print("Hello World!");
+  GstElement *pipeline;
+  GstBus *bus;
 
+  /* Initialize GStreamer */
   gst_init (&argc, &argv);
 
-  gst_version (&major, &minor, &micro, &nano);
+  /* Build the pipeline */
+  pipeline =
+      gst_parse_launch
+      ("playbin uri=https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm",
+      NULL);
 
-  if (nano == 1)
-    nano_str = "(CVS)";
-  else if (nano == 2)
-    nano_str = "(Prerelease)";
-  else
-    nano_str = "";
+  /* Start playing */
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
 
-  printf ("This program is linked against GStreamer %d.%d.%d %s\n",
-          major, minor, micro, nano_str);
+  /* Wait until error or EOS */
+  bus = gst_element_get_bus (pipeline);
+  
+  bool isfinished = false;
+  while (!isfinished) {
+    GstMessage *msg = gst_bus_timed_pop(bus, GST_CLOCK_TIME_NONE);
 
+    if (msg == nullptr) {
+      std::cerr << "msg == nullptr!" << std::endl;
+      //isfinished = true;
+    } else {
+      std::cerr << "Got a msg! msg->type:" << msg->type  << std::endl;
+      /* See next tutorial for proper error message handling/parsing */
+      if (GST_MESSAGE_TYPE (msg) == GST_MESSAGE_ERROR) {
+        std::cout << "An error occurred" << std::endl;
+        g_error ("An error occurred! Re-run with the GST_DEBUG=*:WARN environment "
+            "variable set for more details.");
+        isfinished = true;
+      }
+      gst_message_unref (msg);
+    }
+  }
+  /* Free resources */
+  gst_object_unref (bus);
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  gst_object_unref (pipeline);
+  std::cout << "Finished ok" << std::endl;
   return 0;
 }
